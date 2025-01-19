f5db52ef94fc017a3f2a5a4fe8805b2cf0e69068
Erik van Dam <erikvandam93@gmail.com>
2025-01-19 16:29:05 +0100

    feat: added database migrations step to production deploy

--- apps/migrations/src/index.ts
+++ apps/migrations/src/index.ts
@@ -1,7 +1,12 @@
 import type { Construct } from 'constructs'
-import { MigrationsConstruct } from './constructs/migrations'
 import type { StackProps } from '@internal/cdk-utils/stack'
 import { Stack } from '@internal/cdk-utils/stack'
+import { NodeJSLambda } from '@internal/cdk-utils/lambda'
+import { OutputFormat } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { join } from 'path'
+import { CustomResource, Duration } from 'aws-cdk-lib'
+import { Provider } from 'aws-cdk-lib/custom-resources'
+import { computeHash } from './utils'
 
 type Props = StackProps & {
   databaseUrl: string
@@ -11,8 +16,40 @@ export class MigrationsService extends Stack {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props)
 
-    new MigrationsConstruct(this, 'migrations', {
-      databaseUrl: props.databaseUrl,
+    const migrationDirectoryPath = join(__dirname, './migrations')
+
+    const onEventHandler = new NodeJSLambda(this, 'function', {
+      bundling: {
+        banner: '',
+        format: OutputFormat.CJS,
+        target: 'ES2017',
+        commandHooks: {
+          afterBundling: (_, outputDir: string) => {
+            return [
+              `pwd && mkdir -p ${outputDir}/migrations && cp ${migrationDirectoryPath}/* ${outputDir}/migrations`,
+            ]
+          },
+          beforeBundling: () => [],
+          beforeInstall: () => [],
+        },
+      },
+      entry: join(__dirname, './src/functions/migrations/index.ts'),
+      timeout: Duration.seconds(300),
+      environment: {
+        POSTGRESS_URL: props.databaseUrl,
+      },
+    })
+
+    const provider = new Provider(this, 'provider', {
+      onEventHandler,
+    })
+
+    new CustomResource(this, 'Database:Migrations', {
+      serviceToken: provider.serviceToken,
+      resourceType: 'Custom::DatabaseMigrations',
+      properties: {
+        migrationDirectoryHash: computeHash(migrationDirectoryPath),
+      },
     })
   }
 }
