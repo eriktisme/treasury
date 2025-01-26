cc5be3e50d6f0eebf7b9e7945a4504d5cb30bbc0
Erik van Dam <erikvandam93@gmail.com>
2025-01-26 19:01:29 +0100

    feat: added title to feedback component

--- apps/app/src/features/sign-in/components/EmailSent.tsx
+++ apps/app/src/features/sign-in/components/EmailSent.tsx
@@ -4,8 +4,9 @@ export const EmailSent = () => {
   const { signIn } = useSignIn()
 
   return (
-    <div className="text-center">
-      <p>
+    <div className="flex flex-col gap-6">
+      <h1 className="text-center">Check your email</h1>
+      <p className="text-center">
         We&apos;ve sent you a temporary link.
         <br />
         Please check your inbox at{' '}
