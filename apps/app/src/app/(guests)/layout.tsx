d1c699289c150d7cc81be38bcfe862168dfe2e29
Erik van Dam <erikvandam93@gmail.com>
2025-01-26 19:00:46 +0100

    refactor: whenever there is no redirect url don't fallback when a user is logged in

--- apps/app/src/app/(guests)/layout.tsx
+++ apps/app/src/app/(guests)/layout.tsx
@@ -6,10 +6,9 @@ import { useCallback } from 'react'
 import { useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useAuth } from '@clerk/nextjs'
-import { env } from '@/env'
 import { generateSignInToken } from '@/features/sign-in'
 
-const IsLoggedIn = () => {
+const IsLoggedIn = (props: Readonly<PropsWithChildren>) => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const redirectUrl = searchParams.get('redirect_url')
@@ -19,38 +18,40 @@ const IsLoggedIn = () => {
     async (sessionId: string) => {
       const token = await generateSignInToken(sessionId)
 
-      router.push(
-        `${redirectUrl}?token=${token}` ||
-          env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL
-      )
+      router.push(`${redirectUrl}?token=${token}`)
     },
     [redirectUrl, router]
   )
 
   /**
    * Whenever a user is signed in, redirect to the given redirect url (mobile app)
-   * or when there is no redirect url, redirect to the default after sign in url.
+   * or when there is no redirect url, do nothing.
    */
   useEffect(() => {
+    if (!redirectUrl) {
+      return
+    }
+
     if (isSignedIn && sessionId) {
       void redirectToRedirectUrl(sessionId)
     }
   }, [isSignedIn, redirectToRedirectUrl, redirectUrl, router, sessionId])
 
   if (!isLoaded) {
-    return <div>Loading ...</div>
+    return null
   }
 
-  return <></>
+  return <>{props.children}</>
 }
 
 export default function Layout(props: Readonly<PropsWithChildren>) {
   return (
     <Suspense>
-      <IsLoggedIn />
-      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        {props.children}
-      </div>
+      <IsLoggedIn>
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-neutral-50 p-6 md:p-10">
+          {props.children}
+        </div>
+      </IsLoggedIn>
     </Suspense>
   )
 }
