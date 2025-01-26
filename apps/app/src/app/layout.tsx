87cd0fd71a62d935b03b8e56e90aae4fdcf8ae06
Erik van Dam <erikvandam93@gmail.com>
2025-01-26 19:00:58 +0100

    feat: added clerk loader to prevent re-rendering

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { PropsWithChildren } from 'react'
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from '@clerk/nextjs'
+import { Loader2Icon } from 'lucide-react'
 import { createMetadata } from '@internal/seo'
 
 export const metadata = createMetadata({
@@ -11,7 +12,17 @@ export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className="min-h-screen font-sans antialiased">{children}</body>
+        <body className="min-h-screen font-sans antialiased">
+          <ClerkLoading>
+            <div className="flex min-h-svh flex-1 items-center justify-center">
+              <div className="flex flex-row items-center gap-1.5">
+                <Loader2Icon className="size-5 animate-spin text-neutral-500" />
+                <div className="font-medium text-neutral-700">Loading...</div>
+              </div>
+            </div>
+          </ClerkLoading>
+          <ClerkLoaded>{children}</ClerkLoaded>
+        </body>
       </html>
     </ClerkProvider>
   )
