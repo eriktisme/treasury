c709c25c91361e9d1c9fad781a26e61b50ffa386
Erik van Dam <erikvandam93@gmail.com>
2025-01-26 17:54:43 +0100

    feat: added ability to log in on the mobile app

--- apps/mobile/features/get-started/components/GetStartedButton.tsx
+++ apps/mobile/features/get-started/components/GetStartedButton.tsx
@@ -1,20 +1,50 @@
 import { Button } from 'react-native'
 import * as WebBrowser from 'expo-web-browser'
 import * as Linking from 'expo-linking'
+import * as AuthSession from 'expo-auth-session'
+import { useSignIn } from '@clerk/clerk-expo'
+import { useRouter } from 'expo-router'
 
 export const GetStartedButton = () => {
-  const redirectUri = Linking.createURL('/(authenticated)/dashboard', {
-    scheme: 'treasury',
-  })
+  const router = useRouter()
+
+  const { setActive, signIn } = useSignIn()
 
   const onGettingStarted = async () => {
-    await WebBrowser.openAuthSessionAsync(
-      `${process.env.EXPO_PUBLIC_APP_URL}?redirect_uri=${redirectUri}`,
-      '',
-      {
-        preferEphemeralSession: true,
-      }
+    const redirectUrl = AuthSession.makeRedirectUri({
+      scheme: 'treasury',
+      path: '(guest)/get-started',
+    })
+
+    const result = await WebBrowser.openAuthSessionAsync(
+      `${process.env.EXPO_PUBLIC_APP_URL}/auth/sign-in?redirect_url=${redirectUrl}`
     )
+
+    if (result.type === 'success') {
+      const { queryParams } = Linking.parse(result.url)
+
+      if (!queryParams.token) {
+        // Handle error
+        return
+      }
+
+      const ticket = queryParams.token as string
+
+      const signInAttempt = await signIn.create({
+        strategy: 'ticket',
+        ticket,
+      })
+
+      if (signInAttempt.status === 'complete') {
+        await setActive?.({
+          session: signInAttempt.createdSessionId,
+        })
+
+        return router.push('/(authenticated)/dashboard')
+      }
+
+      // Handle error
+    }
   }
 
   return <Button title="Get Started" onPress={onGettingStarted} />
